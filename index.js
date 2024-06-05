daee2e74d95974d7aceb0324cc9c1f0c4acce4d7
Ackerman-D-Edward <ackermandedward@gmail.com>
2024-06-05 11:17:58 +0100

    Arquivo index do projecto criados , e express e nodemon adicionados como dependencias e escripts start/dev criados

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Olá Mundo')
-})
+const port = 3000;
 
-app.listen(3000)
+app.post('/hello', (req, res) => {
+    res.send('Olá Mundo');
+});
+
+app.listen(port, () => {
+    console.info(`App rodando em http://localhost:${port}`);
+});
