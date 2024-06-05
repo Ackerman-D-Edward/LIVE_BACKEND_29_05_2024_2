365b5de91975d282d5d04ee5bf5ae417302b2a38
Ackerman-D-Edward <ackermandedward@gmail.com>
2024-06-05 12:45:52 +0100

    Body parser adicionado e criação dos endpoint de criação e atualização criados

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.post('/hello', (req, res) => {
     res.send('Olá Mundo');
@@ -35,6 +39,27 @@ app.get('/mensagens/:id', (req, res) => {
 
     res.send(mensagem);
 });
+
+// - [POST] /mensagens - Cria uma nova mensagem
+app.post('/mensagens', (req, res) => {
+    const mensagem = req.body.mensagem;
+
+    mensagens.push(mensagem);
+
+    res.send(`Mensagem criada com sucesso: '${mensagem}'`)
+});
+
+// - [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+app.put('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+
+    const mensagem = req.body.mensagem;
+
+    mensagens[id] = mensagem;
+
+    res.send(`Mensagem atualizada com sucesso: '${mensagem}'.`)
+});
+
 
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}`);
